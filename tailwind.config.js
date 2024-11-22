ae3ab510a0c03eb19c6471b2bf9409aa6e4abcdf
awansanria <virgiawan.dev15@gmail.com>
2024-11-22 11:40:37 +0700

    initial Project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+        colors :{
+            "main":"#F2459F",
+            "second":"#FCDAEC"
+        }
+    },
+  },
+  plugins: [],
+}
+
